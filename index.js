bc228f9a492dfeb0af107eeb45bf27e5527bf75d
nakseono <nakseono@gmail.com>
2021-09-29 23:53:37 +0900

    메시지 받아올 수 있도록 추가

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-// Require the necessary discord.js classes
 const { Client, Intents } = require("discord.js");
 const { token } = require("./config.json");
 
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
 client.once("ready", () => {
   console.log("Ready!");
+});
+
+client.on("message", (message) => {
+  console.log(message);
+  if (message.content === "ping") {
+    message.reply("pong");
+  }
 });
 
 // Login to Discord with your client's token

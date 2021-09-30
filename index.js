0ba481f599d8054a7de106b41016388d2e01939c
nakseono <nakseono@gmail.com>
2021-09-30 22:02:11 +0900

    add todo

--- index.js
+++ index.js
@@ -16,3 +16,7 @@ client.on("message", (message) => {
 
 // Login to Discord with your client's token
 client.login(token);
+
+// 구현해야 할 기능
+// 1. YouTube API랑 연동해서 노래 불러오기
+// 2. LostArk API랑 연동해서 전투정보실 정보 불러오기 (HTML parsing)

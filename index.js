2c6e4cad2defa0e9e7e8fda4d43f9b0405357cc6
nakseono <nakseono@gmail.com>
2021-10-16 23:50:11 +0900

    lostArk 페이지 추가해서 분리작업 시작

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { token, prefix } = require("./config.json");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const { MessageEmbed } = require("discord.js");
+const lostArk = require("./lostArk.js");
 
 const client = new Client({
   intents: [
@@ -15,129 +16,73 @@ const client = new Client({
 });
 
 client.once("ready", () => {
-  console.log("Ready!");
+  console.log("핑크솔져 준비 완료");
 });
 
 client.on("messageCreate", async (message) => {
-  const order = message.content;
-  const loaOrder = order.split(" ");
+  const order = message.data;
+  const loaOrder = message.content.split(" ");
   const loaInfoOrder = loaOrder[0];
-  const nickname = loaOrder[1];
 
   //! 봇 메시지만 제외하고 콘솔에 찍는 기능.
   if (!message.author.bot) {
     console.log(order);
   }
 
-  //! 접두사로 시작하는지 우선적으로 검사
-  if (order.startsWith(prefix)) {
+  //! 봇 메시지가 아니며, 접두사로 시작하는지 우선적으로 검사
+  if (!message.author.bot) {
     if (loaInfoOrder === "!로아") {
-      const loaInfo = await axios.get(
-        `https://lostark.game.onstove.com/Profile/Character/${encodeURI(
-          nickname
-        )}`
-      );
-
-      const $ = cheerio.load(loaInfo.data);
-
-      const userName = $(".profile-character-info__name").text(); //! 닉네임
-
-      const userServer = $(".profile-character-info__server")
-        .text()
-        .replace("@", ""); //! 서버
-
-      const userJob = $(".profile-character-info__img").attr("alt"); //! 직업
-
-      const userGuild = $(".game-info__guild").text().replace("길드", ""); //! 길드
-
-      const userTitle = $(".game-info__title").text().replace("칭호", ""); //! 장착중인 칭호
-
-      const userEngrave = $(
-        "div.profile-ability-engrave > div > div > ul > li > span"
-      ).text(); //! 각인
-      //! 위 내용 중, split을 각각 숫자가 아닌 문자열로 구분지어서 \n 해주면 되지 않을까?
-      //! 이건 특성도 마찬가지. 쭉 리스트업 해야하는 문자들은 이렇게 구분짓자.
-
-      const userArea = $(".game-info__wisdom > span")
-        .text()
-        .replace("영지", ""); //! 영지
-
-      const userAbility = $(".profile-ability-battle > ul > li > span").text(); //! 특성
-
-      const userLevel = $(".level-info2__item")
-        .text()
-        .replace("달성 아이템 레벨", "");
-
-      const userBattleLevel = $(".level-info__item")
-        .text()
-        .replace("전투 레벨", "");
-
-      const userGroupLevel = $(".level-info__expedition")
-        .text()
-        .replace("원정대 레벨", "");
-
-      // 수집형 포인트 $(".").text().replace("", "");
-
-      // 보유 캐릭터 리스트 $(".").text().replace("", "");
-
-      const embedMessage = new MessageEmbed()
-        .setColor("#ff3399")
-        .setTitle(`${userName}`)
-        .addFields(
-          {
-            name: "기본 정보",
-            value: `\`서  버\` : ${userServer}\n\`클래스\` : ${userJob}\n\`길  드\` : ${userGuild}\n\`칭  호\` : ${userTitle}\n\`전  투\` : ${userBattleLevel}\n\`아이템\` : ${userLevel}\n\`원정대\` : ${userGroupLevel}\n\`영  지\` : ${userArea}`,
-            inline: true,
-          },
-          {
-            name: "\u200B",
-            value: "\u200B",
-            inline: true,
-          },
-          {
-            name: "착용 장비",
-            value: `test`,
-            inline: true,
-          }
-        )
-        .addFields(
-          {
-            name: "기본 특성",
-            value: `${userAbility}`,
-            inline: true,
-          },
-          { name: "\u200B", value: "\u200B", inline: true },
-          { name: "각인 효과", value: `${userEngrave}`, inline: true }
-        );
-
-      message.channel.send({ embeds: [embedMessage] });
+      const nickname = loaOrder[1];
+      const data = lostArk.getUserInfo(nickname);
+      // message.channel.send(data[0]);
     }
   }
+
+  //! --------------------------------------------------------
+
+  // if (order.startsWith(prefix)) {
+  //   if (loaInfoOrder === "!로아") {
+  //     const loaInfo = await axios.get(
+  //       `https://lostark.game.onstove.com/Profile/Character/${encodeURI(
+  //         nickname
+  //       )}`
+  //     );
+
+  //     const embedMessage = new MessageEmbed()
+  //       .setColor("#ff3399")
+  //       .setTitle(`${userName}`)
+  //       .addFields(
+  //         {
+  //           name: "기본 정보",
+  //           value: `\`서  버\` : ${userServer}\n\`클래스\` : ${userJob}\n\`길  드\` : ${userGuild}\n\`칭  호\` : ${userTitle}\n\`전  투\` : ${userBattleLevel}\n\`아이템\` : ${userLevel}\n\`원정대\` : ${userGroupLevel}\n\`영  지\` : ${userArea}`,
+  //           inline: true,
+  //         },
+  //         {
+  //           name: "\u200B",
+  //           value: "\u200B",
+  //           inline: true,
+  //         },
+  //         {
+  //           name: "착용 장비",
+  //           value: `test`,
+  //           inline: true,
+  //         }
+  //       );
+
+  //     message.channel.send({ embeds: [embedMessage] });
+  //   }
+  // }
 });
 
 // 그리고 아래에 특성이랑 각인 효과.
 // 수집형 포인트
 // 보유 캐릭터 리스트업
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  const { commandName } = interaction;
-
-  if (commandName === "ping") {
-    await interaction.reply("Pong!");
-  } else if (commandName === "server") {
-    await interaction.reply(
-      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-    );
-  } else if (commandName === "user") {
-    await interaction.reply("User info.");
-  }
-});
-
 // Login to Discord with your client's token
 client.login(token);
 
-// 구현해야 할 기능
-// 1. YouTube API랑 연동해서 노래 불러오기
-// 2. LostArk API랑 연동해서 전투정보실 정보 불러오기 (HTML parsing)
+//? 구현해야 할 기능
+//? 1. 로스트아크 전투정보실 DATA
+//? 2. YouTube API 연동해서 노래 기능까지 추가
+//? 3. 로스트아크 일정표에 맞춰서 모험섬, 카오스게이트 등 알림기능
+//? 4. xlsx 모듈 이용해서 수요눕클회 스프레트 시트에서 일정 안 만진사람 알림가도록

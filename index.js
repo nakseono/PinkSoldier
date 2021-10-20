2701fcef33cbf011b405e4faa44572171372ca1f
nakseono <nakseono@gmail.com>
2021-10-20 20:19:19 +0900

    필요없는 주석 걷어냄

--- index.js
+++ index.js
@@ -32,54 +32,72 @@ client.on("messageCreate", async (message) => {
   //! 봇 메시지가 아니며, 접두사로 시작하는지 우선적으로 검사
   if (!message.author.bot) {
     if (loaInfoOrder === "!로아") {
-      const nickname = loaOrder[1];
-      const userData = lostArk.getUserInfo(nickname);
-      console.log("index.js");
-      console.log(userData);
-      // message.channel.send(`치명 : ${userData["ability"][1]}`);
-      // message.channel.send(data[0]);
+      const userName = loaOrder[1];
+      lostArk.getUserInfo(userName).then((data) => {
+        const embed = createLoaEmbed(userName, data);
+        console.log(`embed: ${embed}`);
+
+        message.channel.send({ embeds: [embed] });
+      });
     }
   }
 
   //! --------------------------------------------------------
 
-  const createLoaEmbed = (userName, data) => {};
+  const createLoaEmbed = (userName, data) => {
+    // userName이랑 data(json형태) 로 넘겨주면
+    //! 특성 정보 가공
+    let abilityBody = "";
 
-  // if (order.startsWith(prefix)) {
-  //   if (loaInfoOrder === "!로아") {
-  //     const loaInfo = await axios.get(
-  //       `https://lostark.game.onstove.com/Profile/Character/${encodeURI(
-  //         nickname
-  //       )}`
-  //     );
+    for (let i = 1; i < data["ability"].length; i++) {
+      if ((i = 1)) {
+        abilityBody += `\`치  명\` : ${data["ability"][i]}\n`;
+      }
+      if ((i = 3)) {
+        abilityBody += `\`특  화\` : ${data["ability"][i]}\n`;
+      }
+      if ((i = 5)) {
+        abilityBody += `\`제  압\` : ${data["ability"][i]}\n`;
+      }
+      if ((i = 7)) {
+        abilityBody += `\`신  속\` : ${data["ability"][i]}\n`;
+      }
+      if ((i = 9)) {
+        abilityBody += `\`인  내\` : ${data["ability"][i]}\n`;
+      }
+      if ((i = 11)) {
+        abilityBody += `\`숙  련\` : ${data["ability"][i]}\n`;
+      }
+    }
 
-  //     const embedMessage = new MessageEmbed()
-  //       .setColor("#ff3399")
-  //       .setTitle(`${userName}`)
-  //       .addFields(
-  //         {
-  //           name: "기본 정보",
-  //           value: `\`서  버\` : ${userServer}\n\`클래스\` : ${userJob}\n\`길  드\` : ${userGuild}\n\`칭  호\` : ${userTitle}\n\`전  투\` : ${userBattleLevel}\n\`아이템\` : ${userLevel}\n\`원정대\` : ${userGroupLevel}\n\`영  지\` : ${userArea}`,
-  //           inline: true,
-  //         },
-  //         {
-  //           name: "\u200B",
-  //           value: "\u200B",
-  //           inline: true,
-  //         },
-  //         {
-  //           name: "착용 장비",
-  //           value: `test`,
-  //           inline: true,
-  //         }
-  //       );
+    //? ----------------------------------------------------
 
-  //     message.channel.send({ embeds: [embedMessage] });
-  //   }
-  // }
+    const embedMessage = new MessageEmbed()
+      .setColor("#ff3399")
+      .setTitle(`${userName}`)
+      .addFields(
+        {
+          name: "기본 정보",
+          value: `\`서  버\` : ${data["server"]}\n\`클래스\` : ${data["job"]}\n\`길  드\` : ${data["guild"]}\n\`칭  호\` : ${data["title"]}\n\`전  투\` : ${data["level"]}\n\`아이템\` : ${data["itemLevel"]}\n\`원정대\` : ${data["garden_level"]}\n\`영  지\` : ${data["garden_name"]}`,
+          inline: true,
+        },
+        {
+          name: "\u200B",
+          value: "\u200B",
+          inline: true,
+        },
+        {
+          name: "착용 장비",
+          value: `test`,
+          inline: true,
+        }
+      )
+      .addFields({ name: "특성 정보", value: `${abilityBody}`, inline: true });
+
+    return embedMessage;
+  };
 });
 
-// Login to Discord with your client's token
 client.login(token);
 
 //? 구현해야 할 기능

749411a0e3bf24b58f1d3318d442e7e0434b8950
nakseono <nakseono@gmail.com>
2021-10-28 18:46:55 +0900

    명령어 리스트 업데이트

--- orderList.js
+++ orderList.js
@@ -1,23 +1,29 @@
 const { MessageEmbed } = require("discord.js");
-const botImage = require("./pinkSoldier.png");
 
 const returnOrderList = () => {
   const embedMessage = new MessageEmbed()
     .setColor("#ff3399")
     .setTitle(`핑크솔져 명령어 리스트`)
-    .setThumbnail(`${botImage}`)
     .addFields(
       {
         name: "!정보 [닉네임]",
-        value: `로스트아크 전투정보실에서 [닉네임]의 데이터를 가져옵니다.\n예시 : \`!정보 김낙서\``,
+        value: `로스트아크 전투정보실에서\n[닉네임]의 데이터를 가져옵니다.\n예시 : \`!정보 김낙서\``,
+        inline: true,
       },
       {
         name: "!로아와 [닉네임]",
-        value: `입력된 [닉네임]에 해당하는 로아와 링크를 출력합니다. \n예시 : \`!로아와 김낙서\``,
-      },
+        value: `입력된 [닉네임]에 해당하는\n로아와 링크를 출력합니다. \n예시 : \`!로아와 김낙서\``,
+        inline: true,
+      }
+    )
+    .addFields(
       {
         name: "!분배 [가격]",
-        value: `입력된 [가격]에 대하여 4인 레이드, 8인 레이드 기준으로 얼마를 입찰하면 n빵이 되는지 계산합니다.\n예시 : \`!분배 24000\``,
+        value: `입력된 [가격]에 대하여 얼마를 입찰하면 n빵이 되는지 계산합니다.\n예시 : \`!분배 24000\``,
+      },
+      {
+        name: "!경매 [가격]",
+        value: `입력된 [가격]에 대하여 최대 얼마까지 입찰해야 이득인지 계산합니다.\n예시 : \`!경매 24000\``,
       }
     );
 

b39d1826941397aabf637a0b762ad860b76c1b9f
nakseono <nakseono@gmail.com>
2021-11-07 20:00:50 +0900

    중복제거

--- lostark/alarmSetting/makeRole.js
+++ lostark/alarmSetting/makeRole.js
@@ -1,16 +1,18 @@
 const makeRole = async (message) => {
-  const temp = await message.guild.roles
-    .create({
-      name: "loaAlarm",
-      color: "BLUE",
-    })
-    .then((data) => {
-      // console.log(`RoleId for loaAlarm : ${data.id}`);
-      return data.id;
-    })
-    .catch(console.error);
+  if (!message.guild.roles.cache.find((role) => role.name === "loaAlarm")) {
+    const temp = await message.guild.roles
+      .create({
+        name: "loaAlarm",
+        color: "BLUE",
+      })
+      .then((data) => {
+        // console.log(`RoleId for loaAlarm : ${data.id}`);
+        return data.id;
+      })
+      .catch(console.error);
 
-  return temp;
+    return temp;
+  }
 };
 
 module.exports = { makeRole };

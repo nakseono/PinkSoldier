09a235ff51dec5551a0ee54c9015b000da94229b
nakseono <nakseono@gmail.com>
2021-11-07 17:01:33 +0900

    채팅채널 생성 후 세팅알림

--- lostark/alarmSetting/makeChannel.js
+++ lostark/alarmSetting/makeChannel.js
@@ -1,7 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+
+const embedMessage = new MessageEmbed()
+  .setColor("#ff3399")
+  .setTitle(`설정 먼저 해주세요!`)
+  .setDescription(
+    `좌측 채널 목록에서 방금 생성된 \`로스트아크-알람\` 채팅 채널을 
+    우클릭 후, \`알림 설정\` - \`@mentions만\` 을 체크해주세요!`
+  );
+
 const makeAlarmChannel = async (message) => {
   const temp = await message.guild.channels
     .create("로스트아크 알람", {
-      type: "GUILD_TEXT",
+      type: "text",
       permissionOverwrites: [
         {
           id: message.guild.roles.everyone,
@@ -12,6 +22,7 @@ const makeAlarmChannel = async (message) => {
     })
     .then((result) => {
       // console.log(`this is channel ID : ${result.id}`);
+      result.send({ embeds: [embedMessage] });
       return result.id;
     });
 

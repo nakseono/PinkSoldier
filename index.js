05b424c785cbeac32460c2e903e949c773d79f92
nakseono <nakseono@gmail.com>
2021-11-08 17:36:08 +0900

    env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, Intents } = require("discord.js");
 
-const { token, prefix } = require("./config.json");
+// const { token, prefix } = require("./config.json");
 const { getUserInfo } = require("./lostark/loaInfo/loaInfoData.js");
 const {
   createLoaInfoEmbed,
@@ -17,6 +17,8 @@ const { makeAlarmChannel } = require("./lostark/alarmSetting/makeChannel");
 const { makeRole } = require("./lostark/alarmSetting/makeRole");
 const { loaAlarm } = require("./lostark/alarmSetting/loaAlarm");
 const { madeNotice, whenStart } = require("./allServerNotice");
+
+const prefix = process.env.PREFIX;
 
 const client = new Client({
   disableEveryone: true,
@@ -102,4 +104,4 @@ client.on("messageCreate", async (message) => {
   }
 });
 
-client.login(token);
+client.login(process.env.TOKEN);

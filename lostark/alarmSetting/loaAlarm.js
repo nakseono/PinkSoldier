91625501b2b03aea3bb7c1f2cef744c619b28e7c
nakseono <nakseono@gmail.com>
2021-11-09 13:38:54 +0900

    55/58분 멘트 변경

--- lostark/alarmSetting/loaAlarm.js
+++ lostark/alarmSetting/loaAlarm.js
@@ -17,13 +17,20 @@ const loaAlarm = (message, client) => {
 
   //? 알람 메시지들 정리.
 
-  const message_MON = `${alarmRoleID}\n\`\`\`diff\n월요일 로스트아크 일정\n\n- 카오스 게이트 & 모험섬 출현 10분 전입니다.\n\n+ 카오스 게이트는 매 정시마다, 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
-  const message_TUE = `${alarmRoleID}\n\`\`\`diff\n화요일 로스트아크 일정\n\n- 필드 보스 & 유령선 & 모험섬 출현 10분 전입니다.\n\n+ 필드 보스와 유령선은 매 정시마다,\n+ 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
-  const message_WED = `${alarmRoleID}\n\`\`\`diff\n수요일 로스트아크 일정\n\n- 모험섬 출현 10분 전입니다.\n\n+ 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
-  const message_THU = `${alarmRoleID}\n\`\`\`diff\n목요일 로스트아크 일정\n\n- 카오스 게이트 & 유령선 & 모험섬 출현 10분 전입니다.\n\n+ 카오스 게이트와 유령선은 매 정시마다,\n+ 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
-  const message_FRI = `${alarmRoleID}\n\`\`\`diff\n금요일 로스트아크 일정\n\n- 필드 보스 & 모험섬 출현 10분 전입니다.\n\n+ 필드 보스는 매 정시마다, 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
-  const message_SAT = `${alarmRoleID}\n\`\`\`diff\n토요일 로스트아크 일정\n\n- 카오스 게이트 & 유령선 & 모험섬 출현 10분 전입니다.\n\n+ 카오스 게이트와 유령선은 매 정시마다,\n+ 모험섬은 9:00, 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\n\n+ 주말 모험섬은 1부 9:00, 11:00, 13:00 \n+ 2부 19:00, 21:00, 23:00 로 나뉘며, 각각 보상 획득이 가능합니다.\`\`\``;
-  const message_SUN = `${alarmRoleID}\n\`\`\`diff\n일요일 로스트아크 일정\n\n- 카오스 게이트 & 필드 보스 & 모험섬 출현 10분 전입니다.\n\n+ 카오스 게이트와 필드 보스는 매 정시마다,\n+ 모험섬은 9:00, 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\n\n+ 주말 모험섬은 1부 9:00, 11:00, 13:00 \n+ 2부 19:00, 21:00, 23:00 로 나뉘며, 각각 보상 획득이 가능합니다.\`\`\``;
+  const message_MON_55 = `${alarmRoleID}\n\`\`\`diff\n월요일 로스트아크 일정\n\n- 카오스 게이트 & 모험섬 출현 10분 전입니다.\n\n+ 카오스 게이트는 매 정시마다, 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
+  const message_MON_58 = `${alarmRoleID}\n\`\`\`diff\n월요일 로스트아크 일정\n\n- 카오스 게이트 & 모험섬 출현 10분 전입니다.\n\n+ 카오스 게이트는 매 정시마다, 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
+  const message_TUE_55 = `${alarmRoleID}\n\`\`\`diff\n화요일 로스트아크 일정\n\n- 필드 보스 & 유령선 & 모험섬 출현 10분 전입니다.\n\n+ 필드 보스와 유령선은 매 정시마다,\n+ 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
+  const message_TUE_58 = `${alarmRoleID}\n\`\`\`diff\n화요일 로스트아크 일정\n\n- 필드 보스 & 유령선 & 모험섬 출현 10분 전입니다.\n\n+ 필드 보스와 유령선은 매 정시마다,\n+ 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
+  const message_WED_55 = `${alarmRoleID}\n\`\`\`diff\n수요일 로스트아크 일정\n\n- 모험섬 출현 10분 전입니다.\n\n+ 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
+  const message_WED_58 = `${alarmRoleID}\n\`\`\`diff\n수요일 로스트아크 일정\n\n- 모험섬 출현 10분 전입니다.\n\n+ 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
+  const message_THU_55 = `${alarmRoleID}\n\`\`\`diff\n목요일 로스트아크 일정\n\n- 카오스 게이트 & 유령선 & 모험섬 출현 10분 전입니다.\n\n+ 카오스 게이트와 유령선은 매 정시마다,\n+ 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
+  const message_THU_58 = `${alarmRoleID}\n\`\`\`diff\n목요일 로스트아크 일정\n\n- 카오스 게이트 & 유령선 & 모험섬 출현 10분 전입니다.\n\n+ 카오스 게이트와 유령선은 매 정시마다,\n+ 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
+  const message_FRI_55 = `${alarmRoleID}\n\`\`\`diff\n금요일 로스트아크 일정\n\n- 필드 보스 & 모험섬 출현 10분 전입니다.\n\n+ 필드 보스는 매 정시마다, 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
+  const message_FRI_58 = `${alarmRoleID}\n\`\`\`diff\n금요일 로스트아크 일정\n\n- 필드 보스 & 모험섬 출현 10분 전입니다.\n\n+ 필드 보스는 매 정시마다, 모험섬은 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\`\`\``;
+  const message_SAT_55 = `${alarmRoleID}\n\`\`\`diff\n토요일 로스트아크 일정\n\n- 카오스 게이트 & 유령선 & 모험섬 출현 10분 전입니다.\n\n+ 카오스 게이트와 유령선은 매 정시마다,\n+ 모험섬은 9:00, 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\n\n+ 주말 모험섬은 1부 9:00, 11:00, 13:00 \n+ 2부 19:00, 21:00, 23:00 로 나뉘며, 각각 보상 획득이 가능합니다.\`\`\``;
+  const message_SAT_58 = `${alarmRoleID}\n\`\`\`diff\n토요일 로스트아크 일정\n\n- 카오스 게이트 & 유령선 & 모험섬 출현 10분 전입니다.\n\n+ 카오스 게이트와 유령선은 매 정시마다,\n+ 모험섬은 9:00, 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\n\n+ 주말 모험섬은 1부 9:00, 11:00, 13:00 \n+ 2부 19:00, 21:00, 23:00 로 나뉘며, 각각 보상 획득이 가능합니다.\`\`\``;
+  const message_SUN_55 = `${alarmRoleID}\n\`\`\`diff\n일요일 로스트아크 일정\n\n- 카오스 게이트 & 필드 보스 & 모험섬 출현 10분 전입니다.\n\n+ 카오스 게이트와 필드 보스는 매 정시마다,\n+ 모험섬은 9:00, 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\n\n+ 주말 모험섬은 1부 9:00, 11:00, 13:00 \n+ 2부 19:00, 21:00, 23:00 로 나뉘며, 각각 보상 획득이 가능합니다.\`\`\``;
+  const message_SUN_58 = `${alarmRoleID}\n\`\`\`diff\n일요일 로스트아크 일정\n\n- 카오스 게이트 & 필드 보스 & 모험섬 출현 10분 전입니다.\n\n+ 카오스 게이트와 필드 보스는 매 정시마다,\n+ 모험섬은 9:00, 11:00, 13:00, 19:00, 21:00, 23:00 에 열립니다.\n\n+ 주말 모험섬은 1부 9:00, 11:00, 13:00 \n+ 2부 19:00, 21:00, 23:00 로 나뉘며, 각각 보상 획득이 가능합니다.\`\`\``;
 
   //? 각 시간별 cron 표현식 정리.
   // [월, 화, 목, 금] 은 시간대가 같으니까 묶자.
@@ -71,101 +78,101 @@ const loaAlarm = (message, client) => {
 
   //! 월
   cron.schedule(rule_MTTF_55(`MON`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_MON);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_MON_55);
   });
   cron.schedule(rule_MTTF_58(`MON`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_MON);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_MON_58);
   });
 
   //! 화
   cron.schedule(rule_MTTF_55(`TUE`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_TUE);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_TUE_55);
   });
   cron.schedule(rule_MTTF_58(`TUE`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_TUE);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_TUE_58);
   });
 
   //! 수 (5개)
   cron.schedule(rule_WED_09_55, () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_WED);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_WED_55);
   });
   cron.schedule(rule_WED_09_58, () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_WED);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_WED_58);
   });
 
   cron.schedule(rule_WED_11_55, () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_WED);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_WED_55);
   });
   cron.schedule(rule_WED_11_58, () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_WED);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_WED_58);
   });
 
   cron.schedule(rule_WED_19_55, () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_WED);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_WED_55);
   });
   cron.schedule(rule_WED_19_58, () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_WED);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_WED_58);
   });
 
   cron.schedule(rule_WED_21_55, () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_WED);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_WED_55);
   });
   cron.schedule(rule_WED_21_58, () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_WED);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_WED_58);
   });
 
   cron.schedule(rule_WED_23_55, () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_WED);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_WED_55);
   });
   cron.schedule(rule_WED_23_58, () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_WED);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_WED_58);
   });
 
   //! 목
   cron.schedule(rule_MTTF_55(`THU`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_THU);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_THU_55);
   });
   cron.schedule(rule_MTTF_58(`THU`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_THU);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_THU_58);
   });
 
   //! 금
   cron.schedule(rule_MTTF_55(`FRI`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_FRI);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_FRI_55);
   });
   cron.schedule(rule_MTTF_58(`FRI`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_FRI);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_FRI_58);
   });
 
   //! 토
   cron.schedule(rule_Weekend_55(`FRI`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_SAT);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_SAT_55);
   });
   cron.schedule(rule_Weekend_58(`FRI`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_SAT);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_SAT_58);
   });
 
   cron.schedule(rule_MTTF_55(`SAT`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_SAT);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_SAT_55);
   });
   cron.schedule(rule_MTTF_58(`SAT`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_SAT);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_SAT_58);
   });
 
   //! 일
 
   cron.schedule(rule_Weekend_55(`SAT`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_SUN);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_SUN_55);
   });
   cron.schedule(rule_Weekend_58(`SAT`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_SUN);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_SUN_58);
   });
 
   cron.schedule(rule_MTTF_55(`SUN`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_SUN);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_SUN_55);
   });
   cron.schedule(rule_MTTF_58(`SUN`), () => {
-    client.channels.cache.get(`${alarmChannelID}`).send(message_SUN);
+    client.channels.cache.get(`${alarmChannelID}`).send(message_SUN_58);
   });
 };
 

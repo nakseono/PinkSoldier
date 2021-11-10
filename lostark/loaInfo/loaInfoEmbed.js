a7d79fc05c487cf32f00e0043c8ba3c3b933ed01
nakseono <nakseono@gmail.com>
2021-11-10 17:55:31 +0900

    정보 메시지 에러처리

--- lostark/loaInfo/loaInfoEmbed.js
+++ lostark/loaInfo/loaInfoEmbed.js
@@ -6,116 +6,140 @@ const createLoaInfoEmbed = async (userName, data, message) => {
 
   //? ------- 기본 특성 정보 가공 -------
 
-  let basicAbilityBody = "";
+  try {
+    let basicAbilityBody = "";
 
-  for (let p = 1; p < data["basic-ability"].length; p++) {
-    if ((p = 1)) {
-      basicAbilityBody += `\`공격력\`: ${data["basic-ability"][p]}\n`;
+    for (let p = 1; p < data["basic-ability"].length; p++) {
+      if ((p = 1)) {
+        basicAbilityBody += `\`공격력\`: ${data["basic-ability"][p]}\n`;
+      }
+      if ((p = 3)) {
+        basicAbilityBody += `\`생명력\`: ${data["basic-ability"][p]}\n`;
+      }
     }
-    if ((p = 3)) {
-      basicAbilityBody += `\`생명력\`: ${data["basic-ability"][p]}\n`;
+
+    //? ------- 전투 특성 정보 가공 -------
+
+    let abilityBody = "";
+
+    for (let i = 1; i < data["ability"].length; i++) {
+      if ((i = 1)) {
+        abilityBody += `\`치  명\` : ${data["ability"][i]}\n`;
+      }
+      if ((i = 3)) {
+        abilityBody += `\`특  화\` : ${data["ability"][i]}\n`;
+      }
+      if ((i = 5)) {
+        abilityBody += `\`제  압\` : ${data["ability"][i]}\n`;
+      }
+      if ((i = 7)) {
+        abilityBody += `\`신  속\` : ${data["ability"][i]}\n`;
+      }
+      if ((i = 9)) {
+        abilityBody += `\`인  내\` : ${data["ability"][i]}\n`;
+      }
+      if ((i = 11)) {
+        abilityBody += `\`숙  련\` : ${data["ability"][i]}\n`;
+      }
     }
-  }
 
-  //? ------- 전투 특성 정보 가공 -------
+    //? ------- 각인 정보 가공 -------
 
-  let abilityBody = "";
+    let engraveBody = "";
 
-  for (let i = 1; i < data["ability"].length; i++) {
-    if ((i = 1)) {
-      abilityBody += `\`치  명\` : ${data["ability"][i]}\n`;
+    for (let k = 0; k < data["engrave"].length; k++) {
+      engraveBody += `${data["engrave"][k]}\n`;
     }
-    if ((i = 3)) {
-      abilityBody += `\`특  화\` : ${data["ability"][i]}\n`;
+
+    //? ------- 썸네일 정보 가공 -------
+
+    let thumbnailURL = "";
+
+    for (let l = 0; l < classImage["job_images"].length; l++) {
+      if (classImage["job_images"][l][`jobName`] === data["job"]) {
+        thumbnailURL = classImage["job_images"][l]["imgSrc"];
+      }
     }
-    if ((i = 5)) {
-      abilityBody += `\`제  압\` : ${data["ability"][i]}\n`;
-    }
-    if ((i = 7)) {
-      abilityBody += `\`신  속\` : ${data["ability"][i]}\n`;
-    }
-    if ((i = 9)) {
-      abilityBody += `\`인  내\` : ${data["ability"][i]}\n`;
-    }
-    if ((i = 11)) {
-      abilityBody += `\`숙  련\` : ${data["ability"][i]}\n`;
-    }
-  }
 
-  //? ------- 각인 정보 가공 -------
+    //? ------- 종합해서 임베드 만들기 -------
 
-  let engraveBody = "";
+    const embedMessage = new MessageEmbed()
+      .setColor("#ff3399")
+      .setTitle(`${userName}`)
+      .setThumbnail(`${thumbnailURL}`)
+      .addFields(
+        {
+          name: "기본 정보",
+          value: `\`서  버\` : ${data["server"]}\n\`클래스\` : ${data["job"]}\n\`길  드\` : ${data["guild"]}\n\`칭  호\` : ${data["title"]}`,
+          inline: true,
+        },
+        {
+          name: "레벨 정보",
+          value: `\`전  투\` : ${data["level"]}\n\`아이템\` : ${data["itemLevel"]}\n\`원정대\` : ${data["userGroupLevel"]}\n\`영  지\` : ${data["garden_level"]}`,
+          inline: true,
+        },
+        {
+          name: "\u200B",
+          value: `\u200B`,
+          inline: true,
+        }
+      )
 
-  for (let k = 0; k < data["engrave"].length; k++) {
-    engraveBody += `${data["engrave"][k]}\n`;
-  }
-
-  //? ------- 썸네일 정보 가공 -------
-
-  let thumbnailURL = "";
-
-  for (let l = 0; l < classImage["job_images"].length; l++) {
-    if (classImage["job_images"][l][`jobName`] === data["job"]) {
-      thumbnailURL = classImage["job_images"][l]["imgSrc"];
-    }
-  }
-
-  //? ------- 종합해서 임베드 만들기 -------
-
-  const embedMessage = new MessageEmbed()
-    .setColor("#ff3399")
-    .setTitle(`${userName}`)
-    .setThumbnail(`${thumbnailURL}`)
-    .addFields(
-      {
-        name: "기본 정보",
-        value: `\`서  버\` : ${data["server"]}\n\`클래스\` : ${data["job"]}\n\`길  드\` : ${data["guild"]}\n\`칭  호\` : ${data["title"]}`,
-        inline: true,
-      },
-      {
-        name: "레벨 정보",
-        value: `\`전  투\` : ${data["level"]}\n\`아이템\` : ${data["itemLevel"]}\n\`원정대\` : ${data["userGroupLevel"]}\n\`영  지\` : ${data["garden_level"]}`,
-        inline: true,
-      },
-      {
+      .addFields({
         name: "\u200B",
-        value: `\u200B`,
-        inline: true,
-      }
-    )
+        value: "\u200B",
+      })
 
-    .addFields({
-      name: "\u200B",
-      value: "\u200B",
-    })
+      .addFields(
+        {
+          name: "기본 특성",
+          value: `${basicAbilityBody}`,
+          inline: true,
+        },
+        { name: "전투 특성", value: `${abilityBody}`, inline: true },
+        {
+          name: "각인 정보",
+          value: `${engraveBody}`,
+          inline: true,
+        }
+      );
 
-    .addFields(
-      {
-        name: "기본 특성",
-        value: `${basicAbilityBody}`,
-        inline: true,
-      },
-      { name: "전투 특성", value: `${abilityBody}`, inline: true },
-      {
-        name: "각인 정보",
-        value: `${engraveBody}`,
-        inline: true,
-      }
-    );
+    const already = await message.channel.messages.fetch({ limit: 1 });
 
-  const already = await message.channel.messages.fetch({ limit: 1 });
+    message.channel.bulkDelete(already);
 
-  message.channel.bulkDelete(already);
-  message.channel.send({ embeds: [embedMessage] });
+    message.channel.send({ embeds: [embedMessage] });
+  } catch (error) {
+    const already = await message.channel.messages.fetch({ limit: 1 });
+
+    message.channel.bulkDelete(already);
+    message.channel.send({ embeds: [errorMessage] });
+
+    console.log(error);
+  }
 };
 
-const createLoawaLinkEmbed = (userName, message) => {
-  const embedMessage = new MessageEmbed().setColor("#ff3399").addFields({
-    name: `링크 클릭시 로아와 페이지로 이동합니다.`,
-    value: `https://loawa.com/char/${userName}`,
-  });
+const createLoawaLinkEmbed = async (userName, message) => {
+  try {
+    const embedMessage = new MessageEmbed().setColor("#ff3399").addFields({
+      name: `링크 클릭시 로아와 페이지로 이동합니다.`,
+      value: `https://loawa.com/char/${userName}`,
+    });
 
-  message.channel.send({ embeds: [embedMessage] });
+    message.channel.send({ embeds: [embedMessage] });
+  } catch (error) {
+    const already = await message.channel.messages.fetch({ limit: 1 });
+
+    message.channel.bulkDelete(already);
+    console.log(error);
+  }
 };
+
+const errorMessage = new MessageEmbed()
+  .setColor("#ff3399")
+  .setTitle(`에러가 발생했습니다!`)
+  .setDescription(
+    `\`!명령어\` 를 통해 다시 한번 용례를 확인해주시고,\n에러가 지속된다면 개발자에게 문의해주세요 XD`
+  );
 
 module.exports = { createLoaInfoEmbed, createLoawaLinkEmbed };

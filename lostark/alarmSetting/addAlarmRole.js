f95850dc11ad7f51ec40258b5ac4caa806e39642
nakseono <nakseono@gmail.com>
2021-11-13 12:45:28 +0900

    알람 역할 부여 ID Array로 바꿈.

--- lostark/alarmSetting/addAlarmRole.js
+++ lostark/alarmSetting/addAlarmRole.js
@@ -1,24 +1,30 @@
 const { MessageEmbed } = require("discord.js");
+const fs = require("fs");
 
 const addEmoji = `⏰`;
 const removeEmoji = `🔕`;
 
 const addRoleEmbed = async (message, client) => {
+  let data = JSON.parse(fs.readFileSync("alarmData.json"));
+
+  let roleArr = [];
+  for (let i = 0; i < data.length; i++) {
+    roleArr[i] = data[i]["role"];
+  }
+
   const embedMessage = new MessageEmbed()
     .setColor("#ff3399")
     .setTitle(`데일리 로아 알림`)
     .setDescription(
-      `데일리 알람을 받고 싶다면 알람시계(${addEmoji}) 이모티콘을 클릭하세요.\n이전에 알람 신청을 했는데 이제는 알람을 받고싶지 않다면 무음모드${removeEmoji} 이모티콘을 클릭하세요.`
+      `데일리 알람을 받고 싶다면 알람시계(${addEmoji}) 이모티콘을 클릭하세요.
+      이전에 알람 신청을 했는데 이제는 알람을 받고싶지 않다면 무음모드${removeEmoji} 이모티콘을 클릭하세요.
+      이 설정 메시지는 \`!알람역할\` 명령어를 통해 언제든지 불러낼 수 있습니다.`
     );
 
   let sendedEmbed = message.channel.send({ embeds: [embedMessage] });
 
   (await sendedEmbed).react(addEmoji);
   (await sendedEmbed).react(removeEmoji);
-
-  let alarmRole = message.guild.roles.cache.find(
-    (role) => role.name === "loaAlarm"
-  );
 
   client.on("messageReactionAdd", async (reaction, user) => {
     if (reaction.message.partial) await reaction.message.fetch();
@@ -28,14 +34,33 @@ const addRoleEmbed = async (message, client) => {
 
     // console.log(JSON.stringify(reaction));
 
-    if (reaction.emoji.name === addEmoji) {
-      reaction.message.guild.members.cache.get(user.id).roles.add(alarmRole);
+    try {
+      if (reaction.emoji.name === addEmoji) {
+        reaction.message.guild.members.cache.get(user.id).roles.add(roleArr);
+      }
+    } catch (error) {
+      console.log(error);
     }
 
-    if (reaction.emoji.name === removeEmoji) {
-      reaction.message.guild.members.cache.get(user.id).roles.remove(alarmRole);
+    try {
+      if (reaction.emoji.name === removeEmoji) {
+        reaction.message.guild.members.cache.get(user.id).roles.remove(roleArr);
+      }
+    } catch (error) {
+      console.log(error);
     }
   });
 };
 
-module.exports = { addRoleEmbed };
+const doAddRoleNotice = (message) => {
+  const embedMessage = new MessageEmbed()
+    .setColor("#ff3399")
+    .setTitle(`역할을 부여받으세요!`)
+    .setDescription(
+      `\`!알람역할\` 명령어를 통해 \`loaAlarm\` 역할을 부여받도록 도와주세요!
+      그리고 \`데일리-로아-알림\` 채널에서 세팅을 마저 끝내주세요! :)`
+    );
+  message.channel.send({ embeds: [embedMessage] });
+};
+
+module.exports = { addRoleEmbed, doAddRoleNotice };

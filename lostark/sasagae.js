abf83702cab463786b8f5be579606a253d66e26c
nakseono <nakseono@gmail.com>
2021-11-14 23:51:36 +0900

    사사게 트러블슈팅

--- lostark/sasagae.js
+++ lostark/sasagae.js
@@ -3,68 +3,72 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const sasagaeEmbed = async (message, userName, errorMessage) => {
-  await axios
-    .get(
-      `https://www.inven.co.kr/board/lostark/5355?query=list&p=1&sterm=&name=subjcont&keyword=${encodeURI(
-        userName
-      )}`
-    )
-    .then((html) => {
-      const $ = cheerio.load(html.data);
+  try {
+    await axios
+      .get(
+        `https://www.inven.co.kr/board/lostark/5355?query=list&p=1&sterm=&name=subjcont&keyword=${encodeURI(
+          userName
+        )}`
+      )
+      .then((html) => {
+        const $ = cheerio.load(html.data);
 
-      let count = 0;
-      let temp = [];
-      let list = [];
+        let count = 0;
+        let temp = [];
+        let list = [];
 
-      $("td.tit > div > div")
-        .children()
-        .each(function (index, item) {
-          if ($(this).attr("class") !== "category") {
-            if (
-              $(this).text() !== undefined &&
-              $(this).text() !== "" &&
-              !$(this).text().includes("이용규칙")
-            ) {
-              temp[count] = $(this).text().trim().split(`\n`);
-              count = count + 1;
+        $("td.tit > div > div")
+          .children()
+          .each(function (index, item) {
+            if ($(this).attr("class") !== "category") {
+              if (
+                $(this).text() !== undefined &&
+                $(this).text() !== "" &&
+                !$(this).text().includes("이용규칙")
+              ) {
+                temp[count] = $(this).text().trim().split(`\n`);
+                count = count + 1;
+              }
             }
-          }
-        });
+          });
 
-      for (let i = 0; i < temp.length; i++) {
-        list.push(temp[i][1].trim());
-      }
-
-      count = 0;
-      temp = [];
-
-      $("td.tit > div > div")
-        .children()
-        .each(function (index, item) {
-          if ($(this).attr("class") === "subject-link") {
-            if ($(this).text() !== undefined && $(this).text() !== "") {
-              temp[count] = $(this).attr("href");
-              count = count + 1;
-            }
-          }
-        });
-
-      let links = temp;
-
-      let arr = [];
-
-      for (let k = 0; k < links.length; k++) {
-        if (list[k] !== undefined) {
-          arr.push(`[${list[k]}](${links[k]})`);
+        for (let i = 0; i < temp.length; i++) {
+          list.push(temp[i][1].trim());
         }
-      }
 
-      if (arr.length === 0) {
-        sasagae(`검색 결과가 존재하지 않습니다!`, userName, message);
-      } else {
-        sasagae(arr.slice(3).join(`\n`), userName, message);
-      }
-    });
+        count = 0;
+        temp = [];
+
+        $("td.tit > div > div")
+          .children()
+          .each(function (index, item) {
+            if ($(this).attr("class") === "subject-link") {
+              if ($(this).text() !== undefined && $(this).text() !== "") {
+                temp[count] = $(this).attr("href");
+                count = count + 1;
+              }
+            }
+          });
+
+        let links = temp;
+
+        let arr = [];
+
+        for (let k = 0; k < links.length; k++) {
+          if (list[k] !== undefined) {
+            arr.push(`[${list[k]}](${links[k]})`);
+          }
+        }
+
+        if (arr.length === 0) {
+          sasagae(`검색 결과가 존재하지 않습니다!`, userName, message);
+        } else {
+          sasagae(arr.slice(3).join(`\n`), userName, message);
+        }
+      });
+  } catch (error) {
+    message.channel.send({ embeds: [errorMessage] });
+  }
 };
 
 const sasagae = async (input, userName, message) => {
